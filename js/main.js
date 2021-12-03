23481b806ad3c84cf543e23725bd3dce8012e3c8
Kojo Boakye-Nimako <boakyeni@usc.edu>
2021-12-02 21:44:43 -0800

    can switch between tabs

--- js/main.js
+++ js/main.js
@@ -1,20 +1,82 @@
-
-
-function navClick(link){
-  /*
-  (?:^|\s) # Match the start of the string or any single whitespace character
-  (?!\S)   # Negative lookahead to verify the above is the whole classname
-         # Ensures there is no non-space character following
-         # (i.e. must be the end of the string or space)
-         this can be replaced with Jquery or Vue
-  */
-  if ( document.getElementById("home").className.match(/(?:^|\s)show active(?!\S)/) ){
-    document.getElementById("home").className = document.getElementById("home").className.replace
-      ( /(?:^|\s)show active(?!\S)/g , '' )
+document.addEventListener("DOMContentLoaded", homeTimeline);
+document.addEventListener("DOMContentLoaded", navTabFunc);
+/*
+This is the functionality for the navigation tabs
+*/
+function navTabFunc(){
+  let navlink1 = document.getElementById("nav-home-tab");
+  let navlink2 = document.getElementById("nav-website-tab");
+  let navlink3 = document.getElementById("nav-art-tab");
+  let navlink4 = document.getElementById("nav-contact-tab");
+  let navbar = [navlink1,navlink2,navlink3,navlink4];
+  let navcontent1 = document.getElementById("home");
+  let navcontent2 = document.getElementById("websites");
+  let navcontent3 = document.getElementById("art-projects");
+  let navcontent4 = document.getElementById("contact");
+  let content = [navcontent1,navcontent2,navcontent3,navcontent4];
+  //that arrow do something
+  for(let i = 0; i < navbar.length; i++){
+    navbar[i].addEventListener("click", () => toggle(navbar[i],content[i]));
   }
-  document.getElementById(link).className += "show active";
 }
+/*
+toggle between tabs on page
+*/
+ function toggle(tab, content){
+   //switches tab
+   removeIdentifiers();
+   addIdentifiers(tab);
+   //switches tab content
+   removeTabContentIdentifiers();
+   addTabContentIdentifiers(content);
+ }
+/*
+removes styling from the previously selected tab
+*/
+ function removeIdentifiers(){
+    let navlink1 = document.getElementById("nav-home-tab");
+    let navlink2 = document.getElementById("nav-website-tab");
+    let navlink3 = document.getElementById("nav-art-tab");
+    let navlink4 = document.getElementById("nav-contact-tab");
+    let navbar = [navlink1,navlink2,navlink3,navlink4];
+    for(let i = 0; i < navbar.length; i++){
+      navbar[i].classList.remove("active");
+      navbar[i].classList.remove("text-white");
+      navbar[i].removeAttribute("style");
+      navbar[i].ariaSelected = false;
+    }
+ }
+/*
+Background is black but bootstrap bg-dark can also be used instead of style
+This add styling to the selected tab
+*/
+ function addIdentifiers(tab){
+   let currentTab = tab;
+   currentTab.classList.add("active");
+   currentTab.classList.add("text-white");
+   currentTab.setAttribute("style","background-color: black;");
+   currentTab.ariaSelected = true;
+ }
+/*
+helper functions for switching tab content
+*/
+ function removeTabContentIdentifiers(){
+   let navcontent1 = document.getElementById("home");
+   let navcontent2 = document.getElementById("websites");
+   let navcontent3 = document.getElementById("art-projects");
+   let navcontent4 = document.getElementById("contact");
+   let content = [navcontent1,navcontent2,navcontent3,navcontent4];
+   for(let i = 0; i < content.length; i++){
+     content[i].classList.remove("show");
+     content[i].classList.remove("active");
+   }
+ }
 
+ function addTabContentIdentifiers(tab){
+   let currentTab = tab;
+   currentTab.classList.add("show");
+   currentTab.classList.add("active");
+ }
 
 /*
 This is the timeline for the animations on the home screen
@@ -49,5 +111,19 @@ function addInvisibleDOM(){
 function makeVisibleDOM(){
 
 }
+/*
+function navClick(link){
+  /*
+  (?:^|\s) # Match the start of the string or any single whitespace character
+  (?!\S)   # Negative lookahead to verify the above is the whole classname
+         # Ensures there is no non-space character following
+         # (i.e. must be the end of the string or space)
+         this can be replaced with Jquery or Vue
 
-document.addEventListener("DOMContentLoaded", homeTimeline);
+  if ( document.getElementById("home").className.match(/(?:^|\s)show active(?!\S)/) ){
+    document.getElementById("home").className = document.getElementById("home").className.replace
+      ( /(?:^|\s)show active(?!\S)/g , '' )
+  }
+  document.getElementById(link).className += "show active";
+}
+*/
